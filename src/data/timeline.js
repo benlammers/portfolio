966eac21c8859bcf43b3e94e19751bc74a0f5bc6
benlammers <21benlammers@gmail.com>
2021-01-07 17:00:51 -0500

    Add Typescript Course to Timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,33 +1,45 @@
 const timelineTypes = {
-   BOOK: "Book",
-   CONFERENCE: "Conference",
-   COURSE: "Course",
-   EMPLOYMENT: "Employment"
+  BOOK: "Book",
+  CONFERENCE: "Conference",
+  COURSE: "Course",
+  EMPLOYMENT: "Employment",
 }
 
 export const timelineData = [
-   {
-      type: timelineTypes.CONFERENCE,
-      date: "Dec 7-9, 2020",
-      title: "Reactathon",
-      description: "Attended workshops and topic tables to interact with industry leaders and learn about complex concepts in React.",
-      link: "https://www.reactathon.com/",
-      linkText: "View Event"
-   },
-   {
-      type: timelineTypes.EMPLOYMENT,
-      date: "May 1, 2020",
-      title: "Centre for Advanced Computing",
-      description: "Started a 16 month software development internship leveraging modern AI solutions in web applications.",
-      link: "https://cac.queensu.ca/",
-      linkText: "View Company"
-   },
-   {
-      type: timelineTypes.COURSE,
-      date: "March 4, 2020",
-      title: "Advanced CSS and Sass",
-      description: "Completed Jonas Schmedtmann's course on advanced and modern CSS including CSS Grid, flexbox and responsive design.",
-      link: "https://www.udemy.com/course/advanced-css-and-sass/",
-      linkText: "View Course"
-   }
+  {
+    type: timelineTypes.COURSE,
+    date: "Jan 7, 2021",
+    title: "Understanding Typescript",
+    description:
+      "Completed Maximilian Schwarzmüller's course on the basics, features and workkflows of Typescript.",
+    link: "https://www.udemy.com/course/understanding-typescript/",
+    linkText: "View Course",
+  },
+  {
+    type: timelineTypes.CONFERENCE,
+    date: "Dec 7-9, 2020",
+    title: "Reactathon",
+    description:
+      "Attended workshops and topic tables to interact with industry leaders and learn about complex concepts in React.",
+    link: "https://www.reactathon.com/",
+    linkText: "View Event",
+  },
+  {
+    type: timelineTypes.EMPLOYMENT,
+    date: "May 1, 2020",
+    title: "Centre for Advanced Computing",
+    description:
+      "Started a 16 month software development internship leveraging modern AI solutions in web applications.",
+    link: "https://cac.queensu.ca/",
+    linkText: "View Company",
+  },
+  {
+    type: timelineTypes.COURSE,
+    date: "March 4, 2020",
+    title: "Advanced CSS and Sass",
+    description:
+      "Completed Jonas Schmedtmann's course on advanced and modern CSS including CSS Grid, flexbox and responsive design.",
+    link: "https://www.udemy.com/course/advanced-css-and-sass/",
+    linkText: "View Course",
+  },
 ]
